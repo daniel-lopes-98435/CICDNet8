b4856e190060e7a91ccbc24e10044b97be81cfd4
daniel-lopes-98435 <43936124+daniel-lopes-98435@users.noreply.github.com>
2025-01-15 19:49:08 -0300

    Add the complete project

--- ContactManagement.Api/Program.cs
+++ ContactManagement.Api/Program.cs
@@ -1,25 +1,20 @@
+using ContactManagement.Api.Configurations;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var configuration = builder.Configuration;
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services
+    .AddApiConfiguration(configuration)
+    //Teste
+    .AddIndentity(configuration)
+    .ResolveDependencies(configuration)
+    .AddSwaggerConfig();
+
+builder.AddCustomLogging();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-//}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
+app.UseApiConfiguration(app.Environment);
 
 app.Run();
